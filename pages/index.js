a7307b9aa7375f964025334095f3941d14f0f8d8
Drielison Lopes <drielisonl@gmail.com>
2021-01-26 00:28:07 -0300

    Primeiros passos
    Onde iniciamos o projeto da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,67 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
+/*
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+function Title(props) {
+  return (
+    <h1>
+      {props.children}
+    </h1>
+  )
+}
+*/
+
+//const BackgroundImage = styled.div`
+//  background-image: url(${db.bg});
+//  flex: 1;
+//  background-size: cover;
+//  background-position: center;
+//`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+      <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>REACT</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Teste seus conhecimentos sobre React!</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Next.js</h1>
+          
+            <p>Teste seus conhecimentos sobre o que aprendemos na imersão-react!</p>
+          </Widget.Content>          
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/DrielisonLopes" />
+    </QuizBackground>
+  );
 }
