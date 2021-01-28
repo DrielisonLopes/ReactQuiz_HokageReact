088d45dfab0fd10d98d07441eec2272c29884d07
Drielison Lopes <drielisonl@gmail.com>
2021-01-28 07:09:04 -0300

    50%
    Linter, Rotas e State.
    Inputs e página do Quiz

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json'
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import Input from '../src/components/Input'
+import Button from '../src/components/Button';
 
 /*
 const Title = styled.h1`
@@ -39,16 +45,37 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}}</title>
+      </Head>
       <QuizContainer>
       <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>REACT</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Teste seus conhecimentos sobre React!</p>
+          <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+               <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Favor, digitar nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Reaja ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
